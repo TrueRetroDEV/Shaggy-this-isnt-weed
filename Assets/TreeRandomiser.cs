d0f3c4bf18312d06d32fa466fba056a695e04e3b
Louis Eriksson (s5213535) <s5213535@bournemouth.ac.uk>
2019-11-06 04:01:41 +0000

    TREES

---
+++ Assets/TreeRandomiser.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeRandomiser : MonoBehaviour {
+
+    void Awake() {
+        transform.localScale += Random.insideUnitSphere.normalized * 2f;
+        transform.eulerAngles = new Vector3(0, Random.Range(0, 360), 0);
+        Destroy(this);
+    }
+}
